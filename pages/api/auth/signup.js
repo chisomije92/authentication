a613bc11dbd5fa98c8d74f98016b095fa80eb2fd
Chisom <chisomije92@gmail.com>
2022-01-26 15:00:22 +0100

    improve signup

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -25,11 +25,19 @@ async function handler(req, res) {
 
   const client = await connectToDatabase();
   const db = client.db();
+  const existingUser = await db.collection("users").findOne({ email: email });
+
+  if (existingUser) {
+    res.status(422).json({ message: "User exists already" });
+    client.close();
+    return;
+  }
   const result = await db
     .collection("users")
     .insertOne({ email: email, password: hashPassword });
 
   res.status(201).json({ message: "User created!" });
+  client.close();
 }
 
 export default handler;
