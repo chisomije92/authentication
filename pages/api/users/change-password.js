cb447d31fd85440060abdb6e1dc5b742e8759e05
Chisom <chisomije92@gmail.com>
2022-01-26 21:01:30 +0100

    implement change password logic and api

---
+++ pages/api/users/change-password.js
@@ -0,0 +1,50 @@
+import { getSession } from "next-auth/react";
+import { hashedPassword, verifyPassword } from "../../../helpers/auth";
+import connectToDatabase from "../../../helpers/db";
+
+async function handler(req, res) {
+  if (req.method !== "PATCH") {
+    return;
+  }
+  const session = await getSession({ req: req });
+
+  if (!session) {
+    res.status(401).json({ message: "Not authenticated" });
+    return;
+  }
+
+  const userEmail = session.user.email;
+  const oldPassword = req.body.oldPassword;
+  const newPassword = req.body.newPassword;
+
+  const client = await connectToDatabase();
+  const usersCollection = client.db().collection("users");
+  const user = await usersCollection.findOne({ email: userEmail });
+
+  if (!user) {
+    res.status(401).json({ message: "User not found!" });
+    client.close();
+    return;
+  }
+
+  const currentPassword = user.password;
+
+  const passwordAreEqual = await verifyPassword(oldPassword, currentPassword);
+
+  if (!passwordAreEqual) {
+    res
+      .status(403)
+      .json({ message: "Permission not granted for invalid password" });
+    client.close();
+    return;
+  }
+
+  const hashPassword = await hashedPassword();
+
+  const result = usersCollection.updateOne(
+    { email: userEmail },
+    { $set: { password: hashPassword } }
+  );
+}
+
+export default handler;
