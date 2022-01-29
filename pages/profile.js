e2ef2867e5aaf798ae71df1b296c14d7f6b17516
Chisom <chisomije92@gmail.com>
2022-01-29 16:33:58 +0100

    add next head to profile page

--- pages/profile.js
+++ pages/profile.js
@@ -1,9 +1,20 @@
 import { getSession } from "next-auth/react";
-import { useContext } from "react";
 import UserProfile from "../components/profile/user-profile";
+import Head from "next/head";
 
 function ProfilePage() {
-  return <UserProfile />;
+  return (
+    <>
+      <Head>
+        <title>Profile</title>
+        <meta
+          name="Profile"
+          content={`You may proceed to change your password as this is your profile page`}
+        />
+      </Head>
+      <UserProfile />
+    </>
+  );
 }
 
 export async function getServerSideProps(context) {
