bd74c30518d2205bd7b4e7e340408793a296c7cb
Chisom <chisomije92@gmail.com>
2022-01-29 16:19:56 +0100

    add next head to app page

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
 import { SessionProvider } from "next-auth/react";
 import Layout from "../components/layout/layout";
+import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps: { session, pageProps } }) {
   return (
     <SessionProvider session={session}>
       <Layout>
+        <Head>
+          <title>Authentication</title>
+          <meta
+            name="viewport"
+            contents="initial-scale=1.0, width=device-width"
+          />
+        </Head>
         <Component {...pageProps} />
       </Layout>
     </SessionProvider>
