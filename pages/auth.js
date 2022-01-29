b7f753f980ab63182c5f6efd70687ecaf87bd80a
Chisom <chisomije92@gmail.com>
2022-01-29 16:26:53 +0100

    use next head in auth page

--- pages/auth.js
+++ pages/auth.js
@@ -1,11 +1,18 @@
 import { getSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import Head from "next/head";
 import AuthForm from "../components/auth/auth-form";
 
 function AuthPage() {
   const [isLoading, setIsLoading] = useState(true);
   const router = useRouter();
+  const headData = (
+    <Head>
+      <title>Sign-in/Login</title>
+      <meta name="authentication" content={`Sign in`} />
+    </Head>
+  );
 
   useEffect(() => {
     getSession().then((session) => {
@@ -20,7 +27,12 @@ function AuthPage() {
   if (isLoading) {
     return <p>Loading...</p>;
   }
-  return <AuthForm />;
+  return (
+    <>
+      {headData}
+      <AuthForm />
+    </>
+  );
 }
 
 export default AuthPage;
